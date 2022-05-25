85689b09246484d9b66dc1139e0538e649a3956f
Mario <mizar2502@aol.com>
2022-05-25 22:37:00 +0200

    Fix code

--- example-1/src/app.ts
+++ example-1/src/app.ts
@@ -1,11 +1,11 @@
 import * as Tone from "tone";
 
-const synth = new Tone.Synth().toDestination();
-synth.triggerAttackRelease("C4", "8n");
-synth.triggerAttackRelease("C5", "8n", 1);
-synth.triggerAttackRelease("C4", "8n", 2);
-synth.triggerAttackRelease("C5", "8n", 3);
-
 document.getElementById("play-button").addEventListener("click", async () => {
 	await Tone.start();
+	const synth = new Tone.Synth().toDestination();
+	var myNow = Tone.now()
+	synth.triggerAttackRelease("C4", "8n", myNow + 1);
+	synth.triggerAttackRelease("C5", "8n", myNow + 2);
+	synth.triggerAttackRelease("C4", "8n", myNow + 3);
+	synth.triggerAttackRelease("C5", "8n", myNow + 4);
 });
