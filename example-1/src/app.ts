d51c9ff372a20d31d51686d1af468c03fbfe2b98
Mario <mizar2502@aol.com>
2022-05-26 01:14:14 +0200

    Refactor

--- example-1/src/app.ts
+++ example-1/src/app.ts
@@ -3,7 +3,7 @@ import * as Tone from "tone";
 document.getElementById("play-button").addEventListener("click", async () => {
 	await Tone.start();
 	const synth = new Tone.Synth().toDestination();
-	var myNow = Tone.now()
+	var myNow = Tone.now();
 	synth.triggerAttackRelease("C4", "8n", myNow + 1);
 	synth.triggerAttackRelease("C5", "8n", myNow + 2);
 	synth.triggerAttackRelease("C4", "8n", myNow + 3);
